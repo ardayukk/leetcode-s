f98f970f3fc1d22864ae8f2ee24b9de417d5b69c
Arda Yuksel <142061976+ardayukk@users.noreply.github.com>
2025-12-24 19:29:31 +0300

    Time: 0 ms (100.00%) | Memory: 12 MB (13.52%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -1,27 +1,53 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        bool isEnd = false;
-        int initialStudentCount = students.size();
-        int ctr = 0;
-        int numbLeft = 0;
-        while(!isEnd){
-            if(students.front() == sandwiches.front()){
-                students.erase(students.begin());
-                sandwiches.erase(sandwiches.begin());
-                numbLeft++;
-                ctr = 0;
+        int count0 = 0;
+        int count1 = 0;
+        int numEat = 0;
+        int n = students.size();
+        for(int i = 0; i < n; i++){
+            if(students[i] == 1){
+                count1++;
             }
             else{
-                int temp = students.at(0);
-                students.erase(students.begin());
-                students.push_back(temp);
-                ctr++;
-            }
-            if(ctr == students.size()){
-                isEnd = true;
+                count0++;
             }
         }
-        return initialStudentCount -numbLeft;
+        queue<int> studq;
+        queue<int> sandq;
+        
+        for(int i = 0; i < n; i++){
+            studq.push(students[i]);
+            sandq.push(sandwiches[i]);
+        }
+        int iter = 0;
+        while(!studq.empty()){
+            if(studq.front() == sandq.front()){
+                numEat++;
+                iter = 0;
+                if(studq.front() == 0){
+                    count0 -= 1;
+                }
+                else{
+                    count1 -= 1;
+                }
+                studq.pop();
+                sandq.pop();
+            }
+            else{
+                iter++;
+                int front = studq.front();
+                studq.pop();
+                studq.push(front);
+
+            }
+
+            if ((sandq.front() == 0 && count0 == 0) ||
+                (sandq.front() == 1 && count1 == 0)) {
+                break;
+            }
+            if (iter == studq.size()) break;
+        }
+        return n - numEat;
     }
 };
