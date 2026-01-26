4be5f8c14d43567ccf872eeaf978a7096cd15620
Arda Yuksel <142061976+ardayukk@users.noreply.github.com>
2026-01-26 20:48:11 +0300

    Time: 3 ms (20.67%) | Memory: 30.4 MB (39.07%) - LeetSync

---
+++ 1839-decode-xored-array/decode-xored-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> result;
+        result.push_back(first);
+        for(int i = 0; i < encoded.size(); i++){
+            int n;
+            n = encoded[i] ^ result[i];
+            result.push_back(n);
+        }
+        return result;
+    }
+};
