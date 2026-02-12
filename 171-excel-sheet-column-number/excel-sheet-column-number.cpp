5d1b7d2e7602c26d8026d15e1dcd322d43c8e6ab
Arda Yuksel <142061976+ardayukk@users.noreply.github.com>
2026-02-13 00:04:35 +0300

    Time: 2 ms (8.40%) | Memory: 8.9 MB (9.46%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -5,13 +5,7 @@ public:
         int n = columnTitle.size();
         
         for(int i = 0; i < n; i++){
-            if(i == n - 1){
-                sum += (columnTitle[i] - 'A' + 1);
-            }
-            else{
-                sum += ((columnTitle[i] - 'A' + 1) * (pow(26,(n - i - 1))));
-            }
-            cout << sum << endl;
+            sum += ((columnTitle[i] - 'A' + 1) * (pow(26,(n - i - 1))));
         }
         return sum;
     }
