3cde2c7215c554fe58e980d38a7445bbb42d4bcb
Arda Yuksel <142061976+ardayukk@users.noreply.github.com>
2026-02-13 00:06:39 +0300

    Time: 0 ms (100.00%) | Memory: 8.9 MB (17.34%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
+    static int p26[7];
+
     int titleToNumber(string columnTitle) {
         int sum = 0;
         int n = columnTitle.size();
-        
+
         for(int i = 0; i < n; i++){
-            sum += ((columnTitle[i] - 'A' + 1) * (pow(26,(n - i - 1))));
+            sum += (columnTitle[i] - 'A' + 1) * p26[n - i - 1];
         }
         return sum;
     }
+};
+
+// Precompute once globally
+int Solution::p26[7] = {
+    1,
+    26,
+    676,
+    17576,
+    456976,
+    11881376,
+    308915776
 };
