29a6c82fb3022b0a89c9ad128fb5b84e11e2c858
Arda Yuksel <142061976+ardayukk@users.noreply.github.com>
2026-07-15 14:26:29 +0200

    Time: 0 ms (100.00%) | Memory: 9.6 MB (46.09%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for(int i = 0; i < s.size(); i += (2*k)){
+            if(i + k + 1 > s.size()){
+                reverse(s.begin() + i, s.end());
+                break;
+            }
+            reverse(s.begin() + i, s.begin() + i + k);
+
+        }
+        return s;
+    }
+};
