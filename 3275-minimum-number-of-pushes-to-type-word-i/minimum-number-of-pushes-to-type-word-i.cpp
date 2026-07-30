19f3edc195a99d526320a69aea7a75c933156c1d
Arda Yuksel <142061976+ardayukk@users.noreply.github.com>
2026-07-30 19:52:09 +0200

    Time: 0 ms (100.00%) | Memory: 8.4 MB (54.67%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> v(26,0);
+        for(auto& c: word){
+            v[static_cast<int>(c- 'a')]++;
+        }
+        sort(v.begin(), v.end());
+
+        int i = 0;
+        while(v[i]== 0) i++;
+        int j = 0;
+        int k = 1;
+        int s = 0;
+        for(; i < 26; i++){
+            s += (k * v[i]);
+            j++;
+            if(j == 8){
+                j = 0;
+                k++;
+            }
+        }
+        return s;
+    }
+};
